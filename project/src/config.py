3b21b07256ce623b2c97d21ca0271589e9c684bd
Jana Backhus <hotarunihon@hotmail.de>
2025-05-08 17:30:34 -0700

    Added a configuration file to handle filenames etc.

---
+++ project/src/config.py
@@ -0,0 +1,25 @@
+MODEL_FOLDER = "model/"
+DATA_FOLDER = "data/"
+DATA_FILE = "clean_census.csv"
+FILENAME_OHE = "onehotencoder.pkl"
+FILENAME_LB = "labelbinarizer.pkl"
+FILENAME_MLMODEL = "randomforestclassifier.pkl"
+
+OUTPUT_COL = "salary"
+
+SCORE_OUTPUT_FILE = "slice_output.txt"
+
+CAT_FEATURES = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+
+
+
